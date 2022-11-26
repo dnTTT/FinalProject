72667673e88eb205bcb6f033459cdbe98fb5a914
dnTTT <henriquevieira1805@hotmail.com>
2022-11-26 20:54:01 +0100

    test process list

---
+++ RAT_FinalProject/process_list.py
@@ -0,0 +1,10 @@
+import wmi
+
+import os
+
+# Running the aforementioned command and saving its output
+output = os.popen('wmic process get description, processid').read()
+
+# Displaying the output
+print(output)
+
